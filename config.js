ca1d7473fa65756a277b9d0b04e770647a812ee7
Iamhenrymuoka076 <167719322+Iamhenrymuoka076@users.noreply.github.com>
2024-07-25 21:57:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VagzM5RGk1Fq
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029VagzM5RGk1Fq9b6BCO0q";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/12b0fb245e6b8ba4bf718.jpg";
 global.devs = "255742862266";
-global.sudo = process.env.SUDO || "255742862266";
-global.owner = process.env.OWNER_NUMBER || "255742862266";
+global.sudo = process.env.SUDO || "2348149953788";
+global.owner = process.env.OWNER_NUMBER || "2348149953788";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "true";
 global.wlcm = process.env.WELCOME || "true";
@@ -42,17 +42,17 @@ global.scan = "https://rb.gy/3keql8";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieU1rM0JKaFg0R1JrUzBMdUdRN0lYVTlLWGdjQTYvUmJBSmliOUJUc2gyYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiM2lMQzU5aExnNUh3VDBtNDBJRi9PNytud0M3cHZhYko3YVREcno3ejNoND0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJRTU1FN3k3QytPc0k3QUtTNXhQdzFtRVZxcEdQbUFOVGlJR3FjOGJtOFZBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJLazg2L3gzd1M0R0VMNyt6dVJYNXViRXIzd3I4Yzd4bkxFbERNUHowVHkwPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImNGRnNUbVRqVUdJM3MwY1Z2d05heGpSQldEMlBUS1FHVllNUTlKeVhVa3c9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InNmS1VzaGp6SkNoNFRLTnI1WVNBa1A0N0d3dUIxVTBSclhjSXhkL1Faejg9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMkhWNzMreWttejhMbnJMK3NOMEhySlNkNk9tam90RjI4WksyUVR5cEwxND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVjU1Umh1U29ETzdYVUIrMlBnbGloZ3Q1MG9OSU5mZndUUU5FK2JpVnIyWT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjNoVDE1RGtPc0dCakVtWmtzM3R6Y3pSeG5BSFBHa1pqSEIwTXRlNGtxWUt6bSs4bldNT0FUa28rSnd2d01JMkJKc0gzT1pxVDhqby9aclVZUHBab0FRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MywiYWR2U2VjcmV0S2V5IjoiRXMzblVreWQ4YkJpMUdFV1pucUw4U256Qmp4dVRjVzdwaTlJZlFjOVNFUT0iLCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXMiOltdLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MCwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sImRldmljZUlkIjoiOVlVajJfMmZSeS1XQUFWc09RVkNLUSIsInBob25lSWQiOiI3NDZkMDhjNC03YWY0LTRmMDktYjNlZS1jYmIwODJlMDU3YzEiLCJpZGVudGl0eUlkIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidUFiRWJxNTRZQUNuUEh5dDBiUE53V2RjTmo4PSJ9LCJyZWdpc3RlcmVkIjp0cnVlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVhTDZBOUFWWjBLM2xucmt0cmo3eHRWcmM4Yz0ifSwicmVnaXN0cmF0aW9uIjp7fSwicGFpcmluZ0NvZGUiOiI1UkVMQk02OCIsIm1lIjp7ImlkIjoiMjM0ODE0OTk1Mzc4ODo3NEBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDT3FVek9BRkVOajJpclVHR0FNZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiWmFpM05RbFczTXd0ZS8zbkFEVTdFZzRDTVZzOU5tYW9mUmFFWEZMem1rQT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiOHF0SkhzMmdaUkZ2dWc0TmVtQzdaekVnaEpsb0pJSHdnVEJJMXAzbWRnOE43czFxOFZSaXI1WVVFeU5DOW8vZmkrbG1UVzRXK1ZRMC9Lc2E5SWZCQnc9PSIsImRldmljZVNpZ25hdHVyZSI6Ii83ZmVDdzhwQkdVcSt6RHZNd2RPUllEYmFhVzZ1UFFDQUVhak94S2F3QzlTQVZuUnkza1BUSENrdEx6Y0szQTF2dmJPQ3pIRmVpcTRMKzduVFRvdEJ3PT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjM0ODE0OTk1Mzc4ODo3NEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJXV290elVKVnR6TUxYdjk1d0ExT3hJT0FqRmJQVFptcUgwV2hGeFM4NXBBIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIxOTQwODYzfQ=="
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "you're using first powerful bot to be released in Tanzania| enjoy your time| this is 𝐕𝐄𝐍𝐎𝐂𝐘𝐁𝐄𝐑 𝐌𝐃",
+  caption: process.env.CAPTION || "you're using first powerful bot to be released in Nigeria| enjoy your time| this is 𝐕𝐄𝐍𝐎𝐂𝐘𝐁𝐄𝐑 𝐌𝐃",
   author: process.env.PACK_AUTHER || "VENOCYBER",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "VENOCYBER-𝐌𝐃",
-  ownername: process.env.OWNER_NAME || "VENOCYBER",
+  botname: process.env.BOT_NAME || "Henry-𝐌𝐃",
+  ownername: process.env.OWNER_NAME || "Henry",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
